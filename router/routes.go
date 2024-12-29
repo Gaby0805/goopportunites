55153a37b111a68084e5ffb096a35db1e0ec16a3
Gaby0805 <gabbiebrabo@gmail.com>
2024-12-29 17:30:46 -0400

    feat: subpackge handler done

--- router/routes.go
+++ router/routes.go
@@ -1,43 +1,19 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/Gaby0805/goopportunites/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func inicializateRouter(router *gin.Engine) {
 	v1 := router.Group("api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK,gin.H{
-				"msg": "get opening",
-			})
-		})
-	
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK,gin.H{
-				"msg": "Post opening",
-			})
-		})
-	
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK,gin.H{
-				"msg": "DELETE opening",
-			})
-		})
-	
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK,gin.H{
-				"msg": "PUT opening",
-			})
-		})
-	
-		v1.GET("/openingS", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK,gin.H{
-				"msg": "get opening",
-			})
-		})
+		v1.GET("/opening", handler.CreateOpeningHandler)
+		v1.POST("/opening", handler.ShowOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningHandler)
+
 	}
 
 }
