5c76eee22b3dbefbe0167820caf8e22501b14e47
Gaby0805 <gabbiebrabo@gmail.com>
2024-12-30 02:07:13 -0400

    feat: logger and schemas done

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"github.com/Gaby0805/goopportunites/config"
 	"github.com/Gaby0805/goopportunites/router"
 )
 
-func main() {
+var (
+	logger *config.Logger
+)
 
+func main() {
+	logger = config.GetLogger("main")
+	// Inicialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config inicialization error: %v", err)
+		return
+	}
 	//Inicialize Router
 	router.Inicialize()
-
-
 }
+
+//TODO: TERMINAR VIDEO H 1:19
